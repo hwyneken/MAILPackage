0074000cff2eca0738cd04b1c4ca2e61766b5663
hwyneken <hwyneken@gmail.com>
2021-06-06 22:01:31 -0500

    First try at documentation

--- R/New_MAIL_Functions.R
+++ R/New_MAIL_Functions.R
@@ -1,3 +1,10 @@
+#' Both MAIL_Full and MAIL_Split use the same basic algorithm.
+#' MAIL_Full and MAIL_Split have preset parameters that control whether or not to split the data, the weight types, the complexity penalties and the method for estimating the error variance.
+#' This basic algorithm is contained in the MAIL function, and this was found to have good coverage under a range of parameter settings.
+#' Other splittings besides 50/50 were tried, and we found good coverage with n/4 or n/logn for the exploratory share in many cases, but using n/8 often resulted in undercoverage.
+
+
+
 MAIL_Full = function(XMat,yVec) {
   res = MAIL(XMat,yVec,"Full",
              firstSOILWeightType = "BIC",
@@ -30,51 +37,51 @@ MAIL = function(XMat,yVec,splitOption,
   # 2) calculate weights
   # 3) construct a candidate matrix
   #browser()
-  
+
   if (verbose==TRUE) {
     print("Step 1: Organize Data")
   }
-  
+
   N = dim(XMat)[1]
   p = dim(XMat)[2]
-  
+
   if (splitOption == "Split") {
     expInds = sample(1:N,size=floor(N/2),replace=FALSE)
-    
+
     xExp = XMat[expInds,]
     xCon = XMat[-1*expInds,]
-    
+
     yExp = yVec[expInds]
     yCon = yVec[-1*expInds]
-    
+
     NExp = dim(xExp)[1]
     pExp = dim(xExp)[2]
-    
+
     NCon = dim(xCon)[1]
-    pCon = dim(xCon)[2]    
+    pCon = dim(xCon)[2]
   }
   else {
     xExp = XMat
     xCon = XMat
-    
+
     yExp = yVec
     yCon = yVec
-    
+
     NExp = dim(xExp)[1]
     pExp = dim(xExp)[2]
-    
+
     NCon = dim(xCon)[1]
-    pCon = dim(xCon)[2]       
+    pCon = dim(xCon)[2]
   }
-  
+
   if (verbose == TRUE) {
     print("Step 2: Run First Model Average")
   }
-  
+
   if (firstSOILWeightType != "ARM") {
     soilRes = SOIL(x=XMat,y=yVec,
                    weight_type=firstSOILWeightType,
-                   psi=firstSOILPsi,family="gaussian",method="union")     
+                   psi=firstSOILPsi,family="gaussian",method="union")
   }
   else {
     soilRes = SOIL(x=XMat,yVec,
@@ -82,50 +89,50 @@ MAIL = function(XMat,yVec,splitOption,
                    psi=firstSOILPsi,family="gaussian",method="union",
                    n_train = ceiling(NExp/2)+4)
   }
-  
+
   if (verbose == TRUE) {
     print("Step 3: Select Variables for the Nested Candidate Set")
   }
-  
+
   allSOILScores = as.numeric(soilRes$importance)
   numModels = min(c(floor(dim(xExp)[1]/2),floor(dim(xExp)[2]/2)))
   soilCutoff = sort(allSOILScores,decreasing=TRUE)[numModels]
-  selectedSet = which(as.numeric(soilRes$importance) >= soilCutoff)  
-  
+  selectedSet = which(as.numeric(soilRes$importance) >= soilCutoff)
+
   if (length(selectedSet) > numModels) {
     #browser()
     selectedSet = selectedSet[order(allSOILScores[selectedSet],decreasing=TRUE)[1:numModels]]
   }
   numSelected = length(selectedSet)
-  
+
   selectedSOILScores = as.numeric(soilRes$importance)[selectedSet]
   selectedSetSorted = selectedSet[order(selectedSOILScores,decreasing=TRUE)]
-  
-  
+
+
   ### create the candidate matrix
   if (verbose == TRUE) {
     print("Step 4: Create Candidate Set, and Smallest Model")
   }
-  
+
   candMat = matrix(0,nrow=numSelected,ncol=p)
   tempVarSet = c()
   for (i in 1:numSelected) {
     tempVarSet = c(tempVarSet,selectedSetSorted[i])
     candMat[i,tempVarSet] = 1
   }
-  
+
   reRunSOIL_SmallestModel = SOIL(x=xExp,y=yExp,family="gaussian",weight_type=smallestModelWeightType,
                                  psi=smallestModelPsi,n_train_bound = numModels + 2,
                                  n_train = numModels + 4,
                                  candidate_models = candMat,method="customize")
-  
+
   minInd = which.max(reRunSOIL_SmallestModel$weight)
-  maxInd = dim(candMat)[1] 
-  
+  maxInd = dim(candMat)[1]
+
   if (verbose == TRUE) {
     print("Step 5: Estimate sigma^2")
   }
-  
+
   ##### Variance Estimation
   if (sigma2EstFunc != "trueValue") {
     tempSigma2Func = get(sigma2EstFunc)
@@ -134,19 +141,19 @@ MAIL = function(XMat,yVec,splitOption,
   else {
     estSigma2 = trueSD^2
   }
-  
+
   ### now that we have an estimate of sigma^2,
   ### we can through out the obviously wrong models that are too large
-  ### choose the number of variables as "largestIndex" - 
+  ### choose the number of variables as "largestIndex" -
   ### in other words choose min AIC-corrected as the cutoff
-  
-  
+
+
   if (verbose == TRUE) {
     print("Step 6: Estimate Final Weights")
   }
-  
+
   origSelectedSet = selectedSet
-  
+
   candMat = candMat[minInd:maxInd,]
   if (minInd == maxInd) {
     candMat = matrix(candMat,nrow=1)
@@ -156,25 +163,25 @@ MAIL = function(XMat,yVec,splitOption,
     if (firstSOILWeightType != "ARM") {
       finalSOIL = SOIL(x=XMat,y=yVec,family="gaussian",weight_type=firstSOILWeightType,
                        psi=firstSOILPsi,
-                       candidate_models = candMat,method="customize")      
+                       candidate_models = candMat,method="customize")
     }
     else {
       finalSOIL = SOIL(x=XMat,y=yVec,family="gaussian",weight_type="ARM",
                        psi=firstSOILPsi,n_train = ceiling(NExp/2)+4,
-                       candidate_models = candMat,method="customize")         
+                       candidate_models = candMat,method="customize")
     }
-    modelWeight = finalSOIL$weight    
+    modelWeight = finalSOIL$weight
   }
-  
+
   if (verbose == TRUE) {
     print("Step 7: Get MAIL Estimates and CI's")
   }
-  
+
   numCand = dim(candMat)[1]
   selectedSet = which(candMat[numCand,] != 0)
   numModels = numCand
   numSelected = length(selectedSet)
-  
+
   tempCoefVec <- rep(0,numSelected)
   tempVarVec <- rep(0,numSelected)
 
@@ -190,25 +197,25 @@ MAIL = function(XMat,yVec,splitOption,
     tempDF = data.frame(y=yCon)
     tempDF = cbind(tempDF,tempX)
     tempM = lm(y~.,data=tempDF)
-    
+
     coefList[[i]] <- coef(summary(tempM))
     covMatList[[i]] <- summary(tempM)$cov.unscaled
   }
-  
-  
+
+
   for (i in 1:numSelected) {
     tempVar = selectedSet[i]
     tempModelInds = which(candMat[,tempVar] != 0)
     smallestModel = min(tempModelInds)
     tempModelWeight = modelWeight[tempModelInds] / sum(modelWeight[tempModelInds])
     numTempInds = length(tempModelInds)
-    
+
     tempCoefVec2 <- rep(0,times=numTempInds)
     tempVarVec2 <- rep(0,times=numTempInds)
     for (j in 1:numTempInds) {
       tempInd = tempModelInds[j]
-      
-      
+
+
       tempCoefVec2[j] <- tempModelWeight[j]*coefList[[tempInd]][paste0("V",tempVar),1]
 
       tempWeight2 = ifelse(tempInd == numCand,
@@ -218,21 +225,21 @@ MAIL = function(XMat,yVec,splitOption,
       tempCovMat <- covMatList[[tempInd]]
       tempVarVec2[j] <- tempWeight2 * diag(tempCovMat)[paste("V",tempVar,sep="")]
     }
-    
+
     tempCoefVec[i] <- sum(tempCoefVec2)
     tempVarVec[i] <- sum(tempVarVec2)
   }
-  
-  tempVarVec <- tempVarVec *  estSigma2 
+
+  tempVarVec <- tempVarVec *  estSigma2
 
   tempCI <- matrix(0,nrow=numSelected,ncol=2)
   tempCI[,1] <- tempCoefVec - 1.96*sqrt(tempVarVec)
   tempCI[,2] <- tempCoefVec + 1.96*sqrt(tempVarVec)
-  
+
   betaHatMA <- rep(0,times=p)
   betaHatMA[selectedSet] <- tempCoefVec
-  
-  
+
+
   resList <- list(tempCI = tempCI,
                  selectedSet = selectedSet,
                  margVar = tempVarVec,
@@ -240,8 +247,8 @@ MAIL = function(XMat,yVec,splitOption,
                  modelWeight = modelWeight,
                  estSigma2 = estSigma2,
                  candMat = candMat,
-                 origSelectedSet = origSelectedSet)   
-  
+                 origSelectedSet = origSelectedSet)
+
   return(resList)
-  
+
 }
