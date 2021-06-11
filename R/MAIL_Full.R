23e161aef2fdf0eef97765cca5d7b452bc83c3ea
hwyneken <hwyneken@gmail.com>
2021-06-10 21:10:58 -0500

    first attempt at sectioned comments for MAIL_Full

--- R/MAIL_Full.R
+++ R/MAIL_Full.R
@@ -1,13 +1,27 @@
-#' type something here
+#' MAIL_Full
+#' 
+#' \code{MAIL_Full} runs MAIL without data splitting
+#' 
+#' This is a specific use of the \code{\link{MAIL}} function. 
+#' The function uses the following arguments with MAIL: 
+#' \itemize{
+#'   \item{splitOption}{"Full"}
+#'   \item{firstSOILWeightType}{"BIC"}
+#' }
+#' @param XMat a matrix
+#' @param yVec a vector
+#' @return resList a list 
 
 MAIL_Full = function(XMat,yVec) {
 
   
-  res = MAIL(XMat,yVec,"Full",
-             firstSOILWeightType = "BIC",
-             smallestModelWeightType = "AIC",
-             firstSOILPsi = 0.5,
-             smallestModelPsi = 0,
-             sigma2EstFunc = "LPM_AIC_CV_50Split")
-  return(res)
+  resList = MAIL(XMat,yVec,
+                 splitOption="Full",
+                 firstSOILWeightType = "BIC",
+                 smallestModelWeightType = "AIC",
+                 firstSOILPsi = 0.5,
+                 smallestModelPsi = 0,
+                 sigma2EstFunc = "LPM_AIC_CV_50Split",
+                 verbose=FALSE)
+  return(resList)
 }
--- R/MAIL_Split.R
+++ R/MAIL_Split.R
@@ -1,9 +1,17 @@
+#' MAIL_Split
+#' 
+#' @param XMat a matrix
+#' @param yVec a vector
+#' @return resList a list 
+
 MAIL_Split = function(XMat,yVec) {
-  res = MAIL(XMat,yVec,"Split",
-             firstSOILWeightType = "BIC",
-             smallestModelWeightType = "AIC",
-             firstSOILPsi = 0.5,
-             smallestModelPsi = 0,
-             sigma2EstFunc = "LPM_AIC_CV_50Split")
-  return(res)
+  resList = MAIL(XMat,yVec,
+                 splitOption="Split",
+                 firstSOILWeightType = "BIC",
+                 smallestModelWeightType = "AIC",
+                 firstSOILPsi = 0.5,
+                 smallestModelPsi = 0,
+                 sigma2EstFunc = "LPM_AIC_CV_50Split",
+                 verbose=FALSE)
+  return(resList)
 }
